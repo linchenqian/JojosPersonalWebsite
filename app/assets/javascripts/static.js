88684464089f3b13443530064c5c0daf5fe350d7
Chenqian Lin <chen_qian_22@hotmail.com>
2017-09-18 01:58:42 -0400

    added background img and typed title

---
+++ app/assets/javascripts/static.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+  new Typed('#jojo', {
+    strings: [
+      "a ^500soft^200ware engin^100e^100er.",
+      "a ^500photo^100graph^50er.", 
+      "^300Jo^60jo."
+    ],
+    startDelay: 1000,
+    typeSpeed: 40,
+    backSpeed: 20
+  });
+});
